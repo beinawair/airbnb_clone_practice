2f61e7bfc29cf798de4f81d78aa4214914c7df39
beinawair <notifier.id@gmail.com>
2022-05-05 14:27:19 +0800

    adding date picker

--- components/Header.js
+++ components/Header.js
@@ -1,36 +1,102 @@
 import Image from 'next/image';
-import { 
+import {
   SearchIcon,
   MenuIcon,
   UserCircleIcon,
   UsersIcon,
-  GlobeAltIcon} from '@heroicons/react/solid'
+  GlobeAltIcon,
+} from '@heroicons/react/solid';
+import { useState } from 'react';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+import { DateRangePicker } from 'react-date-range';
 
 function Header() {
+  const [searchInput, setSearchInput] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [numberOfGuest, setNumberOfGuest] = useState(1);
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const resetInput = () => {
+    setSearchInput('');
+  };
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: 'selection',
+  };
+
   return (
-    <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
-        <div className='relative flex items-center h-10 cursor-pointer my-auto'>
-            <Image src='https://links.papareact.com/qd3'
-                layout='fill'
-                alt='logo'
-                objectFit='contain'
-                objectPosition='left'
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          alt="logo"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+      <div className="flex items-center border-2 rounded-full py-2 md:shadow-sm">
+        <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+          className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+          type="text"
+          placeholder="Start your search"
+        />
+        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+      </div>
+
+      {/*Right Container*/}
+      <div className="flex items-center justify-end space-x-4 text-gray-500">
+        <p className="hidden md:inline-flex cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
+
+      {/*Date Picker Booking*/}
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto mt-10">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={['#FD5B61']}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center border-b mb-4">
+            <h2 className="text-2xl pl-2 flex-grow font-semibold">
+              Number of Guests
+            </h2>
+
+            <UsersIcon className="h-5" />
+            <input
+              value={numberOfGuest}
+              onChange={(e) => setNumberOfGuest(e.target.value)}
+              min={1}
+              type="number"
+              className="w-12 pl-2 text-lg outline-none text-red-400"
             />
-        </div>
-        <div className='flex items-center border-2 rounded-full py-2 md:shadow-sm'>
-          <input className='flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400' type='text' placeholder='Start your search' />
-          <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
-        </div>
-        <div className='flex items-center justify-end space-x-4 text-gray-500'>
-          <p className='hidden md:inline-flex cursor-pointer'>Become a host</p>
-          <GlobeAltIcon className='h-6 cursor-pointer' />
-          <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
-            <MenuIcon className='h-6' />
-            <UserCircleIcon className='h-6' />
+          </div>
+          <div className="flex">
+            <button onClick={resetInput} className="flex-grow text-gray-500">
+              Cancel
+            </button>
+            <button className="flex-grow text-red-400">Search</button>
           </div>
         </div>
+      )}
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
